a4dc95f7d2b48cbe562e1aae7db66eafa70164ad
Elisabeth <elise.doisneau@gmail.com>
2025-04-13 20:55:20 +0200

    Update about.jsx

--- src/components/pages/about/about.jsx
+++ src/components/pages/about/about.jsx
@@ -3,14 +3,16 @@ import photo from '../../../assets/pinkcrop.jpg'
 
 const about = () => {
   return (
-    <> 
-    <div> About</div>
-    <p>We are a coffee shop that serves the best coffee in town.</p>
-    <p>Our mission is to provide our customers with the best coffee experience.</p>
-    <p>We source our coffee beans from the best farms around the world.</p>
-    <p>Our coffee is freshly brewed and served with love.</p>
-    <img src={photo} alt="image" />
+    <>
+      <div className="container flex-column justify-content-center align-items-center text-center mt-5">
+        <h1>About Us</h1>
+        <p>We are passionate about coffee and we want to share that passion with you.</p>
+        <p>Our coffee shop is a place where you can relax, unwind, and enjoy a cup of coffee.</p>
+        <p>We believe that coffee is not just a drink, it's an experience.</p>
+        <p>We want to create a space where you can come and enjoy that experience.</p>
 
+        <img src={photo} alt="image" className='w-100' />
+      </div>
     </>
   )
 }
