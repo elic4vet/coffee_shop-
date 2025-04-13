e827e34869ea68b5fd781c9a406e116cab0e29c1
Elisabeth <elise.doisneau@gmail.com>
2025-04-13 19:21:09 +0200

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Nav from './components/navigation/nav2';
+import About from './components/pages/about/about';
+import Services from './components/pages/services/service';
+import Pricing from './components/pages/pricing/pricing';
+import Contact from './components/pages/contact/contact';
+import './App.css'
+
+function App() {
+
+
+  return (
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/components/pages/services/service.jsx
@@ -0,0 +1,12 @@
+ import React from 'react'
+ 
+ const service = () => {
+   return (
+     <div>service
+    <p>We offer </p>
+     </div>
+      
+   )
+ }
+ 
+ export default service
