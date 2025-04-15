15fb01afaaa4b2d2588b0335c99524578380312b
Elisabeth <elise.doisneau@gmail.com>
2025-04-15 20:20:49 +0200

    about page

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const nav = () => {
   return (
     <div>
-        <nav className="navbar navbar-expand-lg bg-light">
+        <nav className="navbar navbar-expand-lg bg-light fixed-top overflow-hidden shadow-sm">
             <div className="container-fluid">
         <a className="navbar-brand" href="#">Coffee Shop</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
--- src/components/pages/about/about.jsx
+++ src/components/pages/about/about.jsx
@@ -1,18 +1,31 @@
 import React from 'react'
-import photo from '../../../assets/pinkcrop.jpg'
+import about_photo from '../../../assets/pinkcrop.jpg'
 
 const about = () => {
   return (
     <>
-      <div className="container flex-column justify-content-center align-items-center text-center mt-5">
-        <h1>About Us</h1>
-        <p>We are passionate about coffee and we want to share that passion with you.</p>
-        <p>Our coffee shop is a place where you can relax, unwind, and enjoy a cup of coffee.</p>
-        <p>We believe that coffee is not just a drink, it's an experience.</p>
-        <p>We want to create a space where you can come and enjoy that experience.</p>
+      <div className="container flex-column justify-between mt-5 align-items-center">
 
-        <img src={photo} alt="image" className='w-100' />
+        <div className="flex flex-column justify-betweenphoto-container mt-20 mb-5">
+          <img src={about_photo} alt="image" className="w-100 rounded-lg" />
+          <div className="text-start mt-5 mb-5 flex-column justify-between ml-5 w-100">
+            <h3 className='mb-2'>About Us</h3>
+            <p className='mb-1'>We are passionate about coffee and we want to share that passion with you.</p>
+            <p className='mb-1'>Our coffee shop is a place where you can relax, unwind, and enjoy a cup of coffee.</p>
+            <p className='mb-1'>We believe that coffee is not just a drink, it's an experience.</p>
+            <p className='mb-1'>We want to create a space where you can come and enjoy that experience.</p>
+          </div>
+        </div>
+
+        <div className="flex flex-row mt-5 rounded-lg">
+          <div className="flex flex-row justify-content-center align-items-center gap-3 ">
+            <img src={about_photo} alt="" className='w-60 rounded-lg"' />
+            <img src={about_photo} alt="" className='w-60 rounded-lg"' />
+            <img src={about_photo} alt="" className='w-60 rounded-lg"' />
+          </div>
+        </div>
       </div>
+
     </>
   )
 }
